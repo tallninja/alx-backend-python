1528cd7a8987f5ced1a7e8aa57d2b396df292f4b
Ernest Wambua <ernestwambua2@gmail.com>
2023-01-05 17:54:05 +0300

    6-sum_mixed_list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""6-sum_mixed_list"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """returns sum of integers and floats"""
+    return sum(mxd_lst)
