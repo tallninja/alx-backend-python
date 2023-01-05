66c11b2b94b40f19dcc84579d1aad96e0d1f2d9c
Ernest Wambua <ernestwambua2@gmail.com>
2023-01-05 17:43:29 +0300

    5-sum_list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List
+"""5-sum_list"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns the sum of a list of floats"""
+    return sum(input_list)
