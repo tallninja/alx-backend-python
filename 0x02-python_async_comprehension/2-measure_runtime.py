802f54ff2c5392b8e2587a0ec7d2deeaf9c82cab
Ernest Wambua <ernestwambua2@gmail.com>
2023-01-10 12:01:48 +0300

    2-measure_runtime

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 """2-measure_runtime.py"""
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ Asynchronous coroutine that returns the execution time """
+    start = time.time()
+    tasks = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*tasks)
+    return time.time() - start
