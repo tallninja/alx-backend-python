e99a810b163b3e98b94821b9b370bfa06feb531b
tallninja <ernestwambua2@gmail.com>
2024-12-08 18:24:26 +0300

    4-stream_ages.py

---
+++ python-generators-0x00/4-stream_ages.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from seed import connect_to_prodev
+
+def stream_user_ages():
+    pass
+
+# ["def paginate_users", "SELECT * FROM user_data LIMIT", "OFFSET"]
+# ["yield", "return"]
+# ["FROM user_data", "SELECT"]
