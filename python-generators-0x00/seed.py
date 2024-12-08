652e1d5e283ee1688f8492f3f59ce026638af486
tallninja <ernestwambua2@gmail.com>
2024-12-08 17:43:49 +0300

    Task 0

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -3,15 +3,13 @@
 from mysql.connector import connect, Error
 
 DB_HOST = 'localhost'
-DB_USER = 'admin'
+DB_USER = 'root'
 DB_PASSWORD = 'password'
 DB_NAME = 'ALX_prodev'
 
 def connect_db():
     try:
-        with connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD) as connection:
-            print(connection)
-            return connection
+        return connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD)
     except Error as e:
         print(e)
 
@@ -27,8 +25,7 @@ def create_database(connection):
 
 def connect_to_prodev():
     try:
-        with connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME) as connection:
-            return connection
+        return connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
     except Error as e:
         print(e)
 
@@ -36,8 +33,8 @@ def connect_to_prodev():
 def create_table(connection):
     query = '''CREATE TABLE IF NOT EXISTS user_data (
                 user_id VARCHAR(255) PRIMARY KEY, 
-                name VARCHAR NOT NULL, 
-                email VARCHAR NOT NULL,
+                name VARCHAR(255) NOT NULL, 
+                email VARCHAR(255) NOT NULL,
                 age DECIMAL NOT NULL
             )'''
     try:
@@ -50,10 +47,27 @@ def create_table(connection):
 def insert_data(connection, data):
     query = f'''
             INSERT INTO user_data VALUES
-            ({data['uuid']}, {data['name']}, {data['email']}, {data['age']})
+            ("{data['uuid']}", "{data['name']}", "{data['email']}", {data['age']})
             '''
     try:
         with connection.cursor() as cursor:
             cursor.execute(query)
     except Error as e:
         print(e)
+
+
+if __name__ == '__main__':
+    conn1 = connect_db()
+    create_database(conn1)
+    conn2 = connect_to_prodev()
+    create_table(conn2)
+    insert_data(conn2,
+        {
+            "uuid": "2cb9e2c9-bc99-4ed0-b203-4c7f35733578",
+            "name": "Ernest Wambua",
+            "email": "ernest@example.com",
+            "age": 24
+        }
+    )
+    conn1.close()
+    conn2.close()
