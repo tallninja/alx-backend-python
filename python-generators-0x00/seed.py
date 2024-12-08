e47405c864b67be09fc8988993334e3de6b8141d
tallninja <ernestwambua2@gmail.com>
2024-12-08 16:15:33 +0300

    Create DB

---
+++ python-generators-0x00/seed.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from mysql.connector import connect, Error
+
+DB_HOST = 'localhost'
+DB_USER = 'admin'
+DB_PASSWORD = 'password'
+DB_NAME = 'ALX_prodev'
+
+def connect_db():
+    try:
+        with connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD) as connection:
+            print(connection)
+            return connection
+    except Error as e:
+        print(e)
+
+
+def create_database(connection):
+    query = f'CREATE DATABASE IF NOT EXISTS {DB_NAME}'
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+    except Error as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    connection = connect_db()
+    create_database(connection)
+
